63c8b6c3c600313b38637ee46eeaf7da9d1598d5
Isaac Abraham <isaac.abraham@gmail.com>
2020-11-04 17:31:06 +0100

    Make a call to the server

--- src/App.fs
+++ src/App.fs
@@ -8,7 +8,12 @@ let mutable count = 0
 // Get a reference to our button and cast the Element to an HTMLButtonElement
 let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
 
+let getData() = promise {
+    let! data = Fetch.fetch "/api/" []
+    let! text = data.text()
+    myButton.innerText <- text
+}
+
 // Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)!" count
+myButton.onclick <- fun _ -> getData()
+
